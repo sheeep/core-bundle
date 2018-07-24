06c51645110a00caca6b5fe7e43c429684cb96b7
Jim Schmid <jim.schmid@1up.io>
2018-07-24 10:28:11 +0200

    Added empty ResourceOwnerMap class.

---
+++ src/Security/Oauth/ResourceOwnerMap.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Contao\CoreBundle\Security\Oauth;
+
+use HWI\Bundle\OAuthBundle\OAuth\ResourceOwner\GenericOAuth2ResourceOwner;
+use HWI\Bundle\OAuthBundle\Security\Http\ResourceOwnerMap as BaseResourceOwnerMap;
+
+class ResourceOwnerMap extends BaseResourceOwnerMap
+{
+
+}
